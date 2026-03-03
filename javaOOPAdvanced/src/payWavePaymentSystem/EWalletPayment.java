c8b5cbe3699462446ddbcab1b81cd0a6da4b2780
Erudite098 <macalde.kerzania1029@gmail.com>
2026-03-03 21:08:27 +0800

    feat: add e-wallet payment

--- javaOOPAdvanced/src/payWavePaymentSystem/EWalletPayment.java
+++ javaOOPAdvanced/src/payWavePaymentSystem/EWalletPayment.java
@@ -1,5 +1,26 @@
 package payWavePaymentSystem;
 
-public class EWalletPayment {
+public class EWalletPayment implements PaymentMethod{
+	
+	/* Member Variables*/
+	private String walletID;
+	private double balance;
+
+	@Override
+	public boolean processPayment(double amount) {
+		if (this.balance >= amount) {
+			this.balance -= amount;
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public String getPaymentDetails() {
+		String paymentDetails = String.format("EWallet [walletID = %s, balance = %d]", this.walletID, this.balance);
+		return paymentDetails;
+	}
+	
+	
 
 }
