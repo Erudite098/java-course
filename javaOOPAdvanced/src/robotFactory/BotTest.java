c9e51d54bd96035e210509256bb63779bd2e7c7b
Erudite098 <macalde.kerzania1029@gmail.com>
2026-03-03 22:47:09 +0800

    refactor: addlines for readability in robot factory test class

--- javaOOPAdvanced/src/robotFactory/BotTest.java
+++ javaOOPAdvanced/src/robotFactory/BotTest.java
@@ -9,22 +9,26 @@ public class BotTest {
 
 		workerBot.liftCrate();
 		workerBot.liftCrate();
+		System.out.println();
 		
 		workerBot.assemblePart();
 		workerBot.assemblePart();
 		workerBot.assemblePart();
+		System.out.println();
 		
 		workerBot.chargeBattery();
 		System.out.println(workerBot.displayEnergy());
-		
+		System.out.println();
 		
 		/* --- Security Bot Operations --- */
 		SecurityBot securityBot = new SecurityBot();
 		
 		securityBot.patrol();
 		securityBot.patrol();
+		System.out.println();
 		
 		securityBot.soundAlarm();
+		System.out.println();
 		
 		securityBot.recharge();
 		System.out.println(securityBot.displayEnergy());
